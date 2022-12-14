927538e337b7b7a926eb385ed71d766e3f92ce29
iman.akbar <iman.akbar@mncgroup.comv>
2022-12-14 10:10:55 +0700

    okezone video

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -29,18 +29,34 @@ public class Main {
 //        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"emulator-5554");
 //        capabilities.setCapability(MobileCapabilityType.UDID,"M5AIGF000706MZW");
 //        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"ROG Phone 5");
-        capabilities.setCapability(MobileCapabilityType.UDID,"emulator-5554");
-        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"AOSP on IA Emulator");
+//        capabilities.setCapability(MobileCapabilityType.UDID,"emulator-5554");
+//        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"AOSP on IA Emulator");
+        capabilities.setCapability(MobileCapabilityType.UDID,"uguoqkfqbuvouotg");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"POCO M3 Pro 5G");
 
-        capabilities.setCapability(MobileCapabilityType.APP,"C:\\Users\\iman akbar\\Downloads\\baru.apk");
-
+//        capabilities.setCapability(MobileCapabilityType.APP,"C:\\Users\\iman akbar\\Downloads\\baru2.apk");
+        capabilities.setCapability(MobileCapabilityType.APP,"C:\\Users\\iman akbar\\Downloads\\oke.apk");
 
         capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
 
 
         driver  = new AndroidDriver(url, capabilities);
-        driver.findElement(By.id("login")).click();
 
+        Thread.sleep(5000);
+        driver.findElement(By.id("md_buttonDefaultNegative")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//hierarchy/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/androidx.drawerlayout.widget.DrawerLayout[1]/android.view.ViewGroup[1]/android.widget.LinearLayout[1]/android.view.ViewGroup[2]/android.widget.ImageButton[1]")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//hierarchy/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/androidx.drawerlayout.widget.DrawerLayout[1]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.ScrollView[1]/android.widget.LinearLayout[1]/android.widget.LinearLayout[2]/android.widget.LinearLayout[2]/android.widget.LinearLayout[2]/android.widget.LinearLayout[8]/android.widget.TextView[1]")).click();
+        Thread.sleep(10000);
+        driver.findElement(By.xpath("//hierarchy/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/androidx.drawerlayout.widget.DrawerLayout[1]/android.view.ViewGroup[1]/android.widget.FrameLayout[1]/android.view.ViewGroup[1]/androidx.viewpager.widget.ViewPager[2]/android.view.ViewGroup[1]/android.widget.RelativeLayout[1]/android.widget.FrameLayout[1]/android.view.ViewGroup[1]/android.widget.RelativeLayout[1]/android.widget.LinearLayout[1]/android.view.ViewGroup[1]/androidx.recyclerview.widget.RecyclerView[1]/android.widget.LinearLayout[1]/android.widget.LinearLayout[1]/android.widget.RelativeLayout[1]/android.widget.RelativeLayout[1]/android.widget.ImageView[1]")).click();
+        Thread.sleep(5000);
+        driver.findElement(By.id("news_type_video")).click();
+//        Thread.sleep(10000);
+//        driver.findElement(By.id("email")).sendKeys("test@ss.co");
+
+
+//        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
 
 
         try {
