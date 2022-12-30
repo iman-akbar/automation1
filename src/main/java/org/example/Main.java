b3adde47fa5c5bf1a3fae17503ac307890be9734
iman.akbar <iman.akbar@mncgroup.comv>
2022-12-30 10:35:15 +0700

    create config file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,9 +6,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import static io.appium.java_client.touch.WaitOptions.waitOptions;
@@ -19,48 +22,30 @@ public class Main {
     public  static DesiredCapabilities capabilities;
     public static AndroidDriver driver;
     public static URL url;
-    public static void main(String[] args) throws MalformedURLException,InterruptedException {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Properties prop=new Properties();
+        FileInputStream ip= new FileInputStream("H:/tesAppium5/config.properties");
+        prop.load(ip);
 
         System.out.println("Hello world!");
         final String URL_STRING = "http://0.0.0.0:4723/wd/hub";
-
         url = new URL(URL_STRING);
-
-
         capabilities = new DesiredCapabilities();
-
-
-        capabilities.setCapability("autoGrantPermissions", "true");
-
-
-//        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"emulator-5554");
-//        capabilities.setCapability(MobileCapabilityType.UDID,"M5AIGF000706MZW");
-//        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"ROG Phone 5");
-        capabilities.setCapability(MobileCapabilityType.UDID, "emulator-5554");
-        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "AOSP on IA Emulator");
-//        capabilities.setCapability(MobileCapabilityType.UDID,"uguoqkfqbuvouotg");
-//        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"POCO M3 Pro 5G");
-
-//        capabilities.setCapability(MobileCapabilityType.APP,"C:\\Users\\iman akbar\\Downloads\\baru2.apk");
-        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        prop.getProperty("permission");
+        prop.getProperty("emulatorName");
+        prop.getProperty("emulatorUDID");
+        prop.getProperty("automator");
         int x = 100;
         while (x != 0) {
             System.out.println(x);
             capabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\iman akbar\\Downloads\\inno-video (1).apk");
-
-
             driver = new AndroidDriver(url, capabilities);
-
-
-
             Thread.sleep(1000);
             driver.findElement(By.xpath("//hierarchy/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/androidx.compose.ui.platform.ComposeView[1]/android.view.View[1]/android.view.ViewGroup[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[3]/android.widget.FrameLayout[1]/android.view.View[1]")).click();
             driver.findElement(By.id("exo_play_pause")).click();
             driver.findElement(By.id("exo_play_pause")).click();
             Thread.sleep(5000);
             driver.quit();
-
-
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
